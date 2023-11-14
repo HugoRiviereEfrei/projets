310dfb65e07a179725136bb0e3bd48d2949fda6b
crafiouse <crafiouse@gmail.com>
2023-11-14 12:15:17 +0100

    tf-idf tweak

--- Main.py
+++ Main.py
@@ -76,20 +76,20 @@ def enlever():
             f.write(mod)
             
             
-def calculer_frequence_mots(chaine):
-    mots = chaine.split()
+def calculer_frequence_mots(chainee):
     frequence_mots = {}
+
+    mots = chainee.split()
     for mot in mots:
         mot = mot.lower()
         frequence_mots[mot] = frequence_mots.get(mot, 0) + 1
     return frequence_mots
 
 def TF_par_texte():
-    os.chdir("cleaned")
     analyse_textes = {}
-    for texte in os.listdir():
-        with open(texte, "r") as t:
-            analyse_textes[texte]=calculer_frequence_mots(t)
+    for texte in os.listdir("cleaned"):
+        with open(f"cleaned/{texte}", "r", encoding="utf-8") as t:
+            analyse_textes.update({texte: calculer_frequence_mots(t.readlines())})
     return analyse_textes
     
 def calculer_idf(repertoire_corpus):
@@ -112,20 +112,26 @@ def calculer_idf(repertoire_corpus):
         idf_scores[mot]= math.log(nb_doc / nb_documents_contenant_mot[mot])
     return idf_scores
 
+
 def calculer_tf_idf(repertoire_corpus):
     idf_scores = calculer_idf(repertoire_corpus)
-    vecteurs_tfidf = []
-    mots_uniques = set()
+
+    # Parcourir les fichiers dans le répertoire
     for fichier in os.listdir(repertoire_corpus):
-        chemin_fichier = os.path.join(repertoire_corpus, fichier)
-        with open(chemin_fichier, 'r', encoding='utf-8') as f:
-            contenu = f.read()
-            tf_scores = calculer_frequence_mots(contenu)
-            tfidf_scores = {mot: tf_scores[mot] * idf_scores[mot] for mot in tf_scores}
-            mots_uniques.update(set(tfidf_scores.keys()))
-            vecteurs_tfidf.append(tfidf_scores)
-    matrice_tfidf = [[document.get(mot, 0) for mot in mots_uniques] for document in vecteurs_tfidf]
-    return matrice_tfidf
+        if fichier.endswith(".txt"):
+            chemin_fichier = os.path.join(repertoire_corpus, fichier)
+            with open(chemin_fichier, 'r', encoding='utf-8') as f:
+                contenu = f.read()
+                # Calculer les scores TF pour chaque document
+                tf_scores = calculer_frequence_mots(contenu)
+
+                # Calculer les scores TF-IDF en multipliant les scores TF par les scores IDF
+                tfidf_scores = {mot: tf_scores[mot] * idf_scores[mot] for mot in tf_scores}
+
+    return tfidf_scores
+
+
+
 
 def calculer_transposee(matrice):
     nb_lignes = len(matrice)
